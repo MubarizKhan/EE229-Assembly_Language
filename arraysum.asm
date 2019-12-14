cbd700cd7be7c4f5b74b6fa15a8ed9e9d5696f2e
Mubariz Khan <37043727+MubarizKhan@users.noreply.github.com>
2019-12-14 21:31:17 +0500

    Add files via upload

--- arraysum.asm
+++ arraysum.asm
@@ -1,24 +1,22 @@
-include irvine32.inc
+include irvine32.inc 
 
 .data
-    arr byte 1,11,2
+    var1 byte 1,2,3,4,5
     sum byte ?
 
 .code
     main proc
-    xor eax, eax
-    mov esi, OFFSET arr
-    mov ecx, sizeof arr
-    ; mov eax, 0
+        xor eax, eax
+        mov esi, offset var1
+        mov ecx, sizeof var1
 
-    L1:
-        add al, [esi]
-        inc esi
-        loop L1
+        L1:
+            add al, [esi]
+            inc esi
+            loop L1
 
-        ; mov eax, edi
-        
         call writedec
+
     call dumpregs
 
     exit
